23f1f39d337c1f31b08ba7db6c28651de6ecc52f
bo858585 <bo858585@gmail.com>
2015-02-13 16:01:45 +0300

    В админке созданы тестовые пользователи user, custuser, perfuser с соответствующими правами (см. README). Поправлен шаблон входа. Добавлены права доступа в метод dispatch() класса BookingCreate для для соответствующих групп пользователей.

--- Booking/booking/views.py
+++ Booking/booking/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import permission_required
 
 from .models import Booking
 from .forms import BookingForm
@@ -14,6 +15,11 @@ class BookingCreate(LoginRequiredMixin, CreateView):
     fields = ['title', 'text', 'price']
     form_class = BookingForm
     success_url = '/home/'
+
+    @method_decorator(login_required)
+    @method_decorator(permission_required('booking.add_booking', raise_exception=True))
+    def dispatch(self, *args, **kwargs):
+        return super(BookingCreate, self).dispatch(*args, **kwargs)
 
     def form_valid(self, form):
         self.object = form.save(commit=False)

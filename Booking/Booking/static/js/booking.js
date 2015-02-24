06f79446ad2c8d6f48567bc8aea18acbdd22c1e8
bo858585 <bo858585@gmail.com>
2015-02-24 19:54:30 +0300

    Javascript-код приведен к стандарту (до некоторой степени) через jslint.

--- Booking/Booking/static/js/booking.js
+++ Booking/Booking/static/js/booking.js
@@ -1,39 +1,40 @@
-$( document ).ready(function() {
-    var xhr;
+$(document).ready(function () {
+  'use strict';
+  var xhr;
 
-    $( "button.serve, button.complete").on('click', function( event ) {
-        event.preventDefault();
-        event.stopPropagation();
+  $("button.serve, button.complete").on('click', function (event) {
+    event.preventDefault();
+    event.stopPropagation();
 
-        if(xhr && xhr.readyState != 4){
-            xhr.abort();
-        }
+    if (xhr && xhr.readyState !== 4) {
+      xhr.abort();
+    }
 
-        var value = $(this).prev().val();
-        var csrfmiddlewaretoken = $(this).prev().prev().val();
-        var url_part = "";
+    var value = $(this).prev().val(),
+    csrfmiddlewaretoken = $(this).prev().prev().val(),
+    url_part = "";
 
-        if ($(this).hasClass('serve')) {
-            url_part = "serve/";
-        } else {
-            if ($(this).hasClass('complete')) {
-                url_part = "complete/";
-            }
-        }
+    if ($(this).hasClass('serve')) {
+      url_part = "serve/";
+    } else {
+      if ($(this).hasClass('complete')) {
+        url_part = "complete/";
+      }
+    }
 
-        xhr = $.ajax({
-            type: "POST",
-            url: "/booking/" + url_part,
-            data: { "id": value, "csrfmiddlewaretoken": csrfmiddlewaretoken },
-            dataType: "json",
-            success: function(msg){
-                $("div.alert").remove();
-                $("div#greeting").after('<div class="alert alert-info">' + msg["request_status"] + '</div>');
-                return false;
-            },
-            error: function(jqXHR, textStatus, errorThrown){
-                console.log( "Error", jqXHR, textStatus, errorThrown);
-            }
-          });
+    xhr = $.ajax({
+      type: "POST",
+      url: "/booking/" + url_part,
+      data: { "id": value, "csrfmiddlewaretoken": csrfmiddlewaretoken },
+      dataType: "json",
+      success: function (msg) {
+        $("div.alert").remove();
+        $("div#greeting").after('<div class="alert alert-info">' + msg.request_status + '</div>');
+        return false;
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.log("Error", jqXHR, textStatus, errorThrown);
+      }
     });
+  });
 });
